d72ba27c19f715fa4ebf927835dbf79f0d26d0e9
Daan Ferdinandusse <daanferdinandusse@earlybird.com>
2024-03-29 17:30:39 +0100

    inital file structure

---
+++ database/interaction_utils.py
@@ -0,0 +1,12 @@
+# Standard
+import logging
+
+
+def main(test: None = None):
+    """TODO write comments, change function name"""
+    pass
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
