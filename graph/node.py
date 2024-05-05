33ead7d24f811889bd520184a980309c6ddf6f1c
Daan Ferdinandusse <daanferdinandusse@earlybird.com>
2024-05-05 10:28:23 +0200

    updated: docs and typing

--- graph/node.py
+++ graph/node.py
@@ -1,6 +1,7 @@
 # Standard
 import logging
 from dataclasses import dataclass, field
+from typing import Callable, Any, Literal
 
 # Node functions
 from graph.node_name_to_function_map import NODE_NAME_TO_FUNCTION_MAP
@@ -8,20 +9,36 @@ from graph.node_name_to_function_map import NODE_NAME_TO_FUNCTION_MAP
 
 @dataclass
 class Node:
-    """TODO class description"""
+    """
+    The node class represents a single step in the scraping process. The node is executable with
+    three types of inputs:
+        - Static inputs: These are the configurations of the scraping step specified by the developer.
+            (e.g., the URL to scrape, the CSS selector to use, etc.)
+        - Dynamic inputs: These are the outputs of the previous nodes in the graph.
+            (e.g. a specific element extracted from the HTML)
+        - Shared inputs: These are objects shared between nodes managed by the scraping graph.
+            (e.g. the browser, the result object, etc.)
+
+    :param id: The unique identifier of the node.
+    :param function: The function to execute when the node is called.
+    :param static_inputs: Configuration of the scraping step.
+    :param dynamic_inputs: Outputs of the previous nodes in the graph.
+    :param shared_inputs: Inputs shared between nodes.
+    :param node_type: The type of the node. (e.g. "regular", "navigation").
+    """
     id: int
-    function: callable
+    function: Callable[..., Any]
     static_inputs: dict[str, any]
-    node_type: str = field(default="regular")
     dynamic_inputs: dict[str, any] = field(default_factory=dict)
     shared_inputs: dict[str, any] = field(default_factory=dict)
+    node_type: Literal["regular", "navigation"] = field(default="regular")
 
-    def __call__(self):
+    def __call__(self) -> Any:
         """Execute the function of the node."""
         function_inputs = {**self.static_inputs, **self.dynamic_inputs, **self.shared_inputs}
         return self.function(**function_inputs)
 
-    def serialize(self):
+    def serialize(self) -> dict[str, Any]:
         """Transform the node to a dictionary representation. Only the static inputs are serialized."""
         return {
             "id": self.id,
@@ -31,7 +48,7 @@ class Node:
         }
 
     @classmethod
-    def deserialize(cls, node_dict: dict[str, any]):
+    def deserialize(cls, node_dict: dict[str, Any]):
         """Compose a node from a dictionary representation."""
         return cls(
             id=node_dict["id"],
@@ -39,11 +56,6 @@ class Node:
             static_inputs=node_dict["static_inputs"],
             node_type=node_dict["node_type"],
         )
-
-
-class NavigationNode(Node):
-    """TODO class description"""
-    node_type: str = field(default="navigation")
 
 
 if __name__ == "__main__":
--- graph/scraper_graph.py
+++ graph/scraper_graph.py
@@ -1,21 +1,24 @@
 # Standard
 import json
 import logging
+from typing import Any
 from collections import defaultdict
 
 # Node dataclasses
-from graph.node import Node, NavigationNode
+from graph.node import Node
 
 # Playwright
 from playwright.sync_api import sync_playwright, Browser, Page
 
 
 class ScraperGraph:
-    """TODO write class description"""
+    """
+    Class representing a graph of nodes. Each node is an executable step in the scraping process.
+    The graph is executed in a depth-first manner, passing the output of each node to its children.
+    """
     def __init__(self):
-        """TODO write method description"""
+        """Initializes an empty graph. Nodes and edges are stored as dictionaries."""
         self.nodes: dict[int, Node] = {}
-        # TODO transform into something with faster lookup (e.g. dict, of ...), while still serializable
         self.edges: dict[int, list[int]] = defaultdict(list)
 
         # , play_wright_page: Page, result_object: any
@@ -23,7 +26,10 @@ class ScraperGraph:
         # self.result_object = result_object
 
     def execute(self):
-        """Execute the steps of the graph in a depth-first manner."""
+        """
+        Execute the steps of the graph in a depth-first manner. Each node is executed once,
+        passing the output to its children.
+        """
         # Add the first node to the node stack, which will be executed first
         nodes_stack: list[Node] = [self.nodes[1]]
 
@@ -31,7 +37,7 @@ class ScraperGraph:
         while nodes_stack:
             node: Node = nodes_stack.pop()
             # Execute the node function with the inputs
-            node_output: any = node()
+            node_output: Any = node()
 
             for child_id in self.edges[node.id]:
                 child_node: Node = self.nodes[child_id]
@@ -49,7 +55,7 @@ class ScraperGraph:
         """Add an edge to the graph."""
         self.edges[parent_node.id].append(child_node.id)
 
-    def serialize(self):
+    def serialize(self) -> dict[str, Any]:
         """Transform the graph to a dictionary representation."""
         return {
             "nodes": {i: node.serialize() for i, node in self.nodes.items()},
@@ -61,10 +67,10 @@ class ScraperGraph:
         with open(file_path, "w") as file:
             json.dump(self.serialize(), file)
 
-    def deserialize_node(self, node_dict: dict[str, any]) -> Node:
+    def deserialize_node(self, node_dict: dict[str, Any]) -> Node:
         """Compose a node from a dictionary representation."""
         if node_dict.get("node_type") == "navigation":
-            node = NavigationNode.deserialize(node_dict)
+            node = Node.deserialize(node_dict)
             # node.page = self.page
             return node
         elif node_dict.get("node_type") == "regular":
@@ -73,7 +79,7 @@ class ScraperGraph:
             raise ValueError(f"Invalid node type: {node_dict.get('node_type')}")
 
     @classmethod
-    def deserialize(cls, graph_dict: dict[str, any]) -> "ScraperGraph":
+    def deserialize(cls, graph_dict: dict[str, Any]) -> "ScraperGraph":
         """Compose a graph from a dictionary representation."""
         graph = cls()
         for node_dict in graph_dict["nodes"].values():
@@ -98,7 +104,7 @@ if __name__ == "__main__":
     # Create a graph
     graph = ScraperGraph()
     node1 = Node(id=1, function=pass_through, static_inputs={})
-    node2 = NavigationNode(id=2, function=pass_through, static_inputs={})
+    node2 = Node(id=2, function=pass_through, static_inputs={}, node_type="navigation")
     graph.add_node(node1)
     graph.add_node(node2)
     graph.add_edge(node1, node2)
